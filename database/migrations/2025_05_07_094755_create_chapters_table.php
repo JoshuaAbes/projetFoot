b869cc3afd07be70369d9fd80c402f991c585366
JoshuaAbes <joshua.abessolo@heig-vd.ch>
2025-05-07 11:49:25 +0200

    add models and migrations for stories, chapters, choices, and progress

---
+++ database/migrations/2025_05_07_094755_create_chapters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('story_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_ending')->default(false);
+            $table->boolean('is_starting')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
